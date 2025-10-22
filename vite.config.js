b2da7d85f33e6bd7993ab699419c92bbed251554
Linkon Miyan <rudlinkon@gmail.com>
2025-10-22 15:21:39 +0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '',
-    assetsDir: 'assets',
-    // Ensure relative paths in production
-    base: './'
-  },
-  server: {
-    // Development server configuration
-    proxy: {
-      '/backend': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/backend/, '')
-      }
+    plugins: [react()],
+    build: {
+        outDir: '',
+        assetsDir: 'assets',
+        // Ensure relative paths in production
+        base: './',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/index.js',
+                assetFileNames: 'assets/index.css',
+            }
+        }
+    },
+    server: {
+        // Development server configuration
+        proxy: {
+            '/backend': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/backend/, '')
+            }
+        }
     }
-  }
 })
